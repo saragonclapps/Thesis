5bac9b1f0bc383125b4f7fa4ead29fbfebaee30b
CRIS_DESKTOP\chigo <cristian.vigo@davinci.edu.ar>
2018-08-04 20:58:19 -0300

    Aim Animations 1.0

--- Assets/Scripts/ReWorkedScripts/Player/Controller/States/AimingState.cs
+++ Assets/Scripts/ReWorkedScripts/Player/Controller/States/AimingState.cs
@@ -24,8 +24,8 @@ namespace Player
             transform = t;
             _mainCamera = mainCamera.transform;
             _anim = anim;
-            _horizontalSpeed = speed/2;
-            _verticalSpeed = speed;
+            _horizontalSpeed = speed/6;
+            _verticalSpeed = speed/3;
         }
 
         public void Enter()
@@ -36,14 +36,13 @@ namespace Player
 
         public void Execute()
         {
-            
-            
             _horizontal = GameInput.instance.horizontalMove;
             _vertical = GameInput.instance.verticalMove;
 
             var moveVector = new Vector2(_horizontal, _vertical).normalized;
 
             _anim.SetFloat("horizontalSpeed", moveVector.x * 90);
+            _anim.SetFloat("verticalSpeed", moveVector.y * 90);
 
             transform.position += transform.forward * moveVector.y * _verticalSpeed * Time.deltaTime;
             transform.position += transform.right * moveVector.x * _horizontalSpeed * Time.deltaTime;
