3c217f450249f8d633952d9a35dcc2330c44b591
steve aragon <stevearagon92@gmail.com>
2018-10-01 17:46:15 -0300

    updates levels

---
+++ Assets/Art/AnimationsSwitch/PlayAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour {
+
+    public Weight pesimeter;
+    private Animator _animator;
+
+    void OnEnable() {
+
+        _animator = GetComponent<Animator>();
+        if (pesimeter != null && _animator != null)
+        {
+            pesimeter.AddOnWeightEnterEvent(() => { if (pesimeter.isActiveAndEnabled) _animator.Play("AnimationPlatform");  });
+            pesimeter.AddOnWeightExitEvent(() => { if (pesimeter.isActiveAndEnabled) _animator.Play("AnimationPlatform"); });
+            pesimeter.AddOnWeightEvent(() => { if (pesimeter.isActiveAndEnabled) { _animator.speed = 1; } else { _animator.speed = 0; } });
+            _animator.speed = 0;
+        }
+    }
+
+    public void StopAnimator(){
+        if (pesimeter != null && _animator != null)
+        {
+            _animator.speed = 0;
+        }
+    }
+}
