48400cef3b7c913d198e1d7999b0dfaecc61884d
CRIS_DESKTOP\chigo <cristian.vigo@davinci.edu.ar>
2018-11-18 15:52:30 -0300

    New Models Dilating Door

---
+++ Assets/Scripts/ReWorkedScripts/Puzzles/Objects/Mobile Wall/WallMobile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMobile : MonoBehaviour {
+
+    public float speedModifier;
+    Animator _anim;
+	void Start ()
+    {
+        _anim = GetComponent<Animator>();
+        _anim.SetFloat("speed", speedModifier);
+	}
+	
+	
+}
